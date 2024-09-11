d28536a09f3448b154ef34bc4b303a9ac6a37e70
Luis  Rodrigues <163193019+devminguel@users.noreply.github.com>
2024-09-11 19:47:45 -0300

    Add files via upload

---
+++ Programa 1/Programa 1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Programa 1
+//
+//  Created by Usuário Convidado on 11/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtPeso: UITextField!
+    
+    @IBOutlet weak var txtAltura: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calcular(_ sender: Any) {
+        //print( "Bom dia 2SIPF")
+        let a = Atleta()
+        a.altura = Float(txtAltura.text!)!
+        a.peso = Float(txtPeso.text!)!
+        //print(a.calcularaImc())
+        let alerta = UIAlertController(
+            title: "Aviso",
+            message: "O seu imc e \(a.calcularaImc()), bom treino",
+            preferredStyle: UIAlertController.Style.alert)
+        
+        alerta.addAction(UIAlertAction(
+                title:"Ok!",
+                style: UIAlertAction.Style.default))
+        
+        present(alerta, animated: true)
+    }
+    
+}
+
